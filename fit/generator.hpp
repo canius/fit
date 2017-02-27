c940e3cc622043983427d907985275b09dbe0a6f
canius.chu <canius.chu@outlook.com>
2017-02-27 15:46:45 +0800

    init

---
+++ fit/generator.hpp
@@ -0,0 +1,16 @@
+//
+//  generator.hpp
+//  fit
+//
+//  Created by canius.chu on 27/2/2017.
+//  Copyright © 2017 EF. All rights reserved.
+//
+
+#ifndef generator_hpp
+#define generator_hpp
+
+#include <stdio.h>
+
+void test();
+
+#endif /* generator_hpp */
---
+++ fit/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  fit
+//
+//  Created by canius.chu on 27/2/2017.
+//  Copyright © 2017 EF. All rights reserved.
+//
+
+#include <iostream>
+#include "mpfit.h"
+#include "generator.hpp"
+
+int main(int argc, const char * argv[]) {
+
+    test();
+    return 0;
+}
