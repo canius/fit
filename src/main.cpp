da4644d280c7cc0be7b8aeb5d96a0a21f23b9ee4
Canius Chu <canius.chu@outlook.com>
2017-02-28 21:43:41 +0800

    update

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  fit
+//
+//  Created by canius.chu on 27/2/2017.
+//  Copyright ? 2017 canius.chu. All rights reserved.
+//
+
+#include "runner.hpp"
+
+int main(int argc, const char * argv[])
+{
+	run(1000000);
+    return 0;
+}
+
